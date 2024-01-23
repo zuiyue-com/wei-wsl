3524626b0d15de44f160440fcab9a7f857bb1e89
HuangWei <market@zuiyue.com>
2024-01-23 18:06:01 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,49 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     wei_windows::init();
     wei_env::bin_init("wei-wsl");
+
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+    let args = args.join("\", \"");
+    let args = &format!("[\"{}\"]", args);
     
+    for _ in 0..3 {
+        let wsl_url = match std::fs::read_to_string("./wsl.dat") {
+            Ok(c) => c,
+            Err(_) => {
+                run_server()?;
+                continue;
+            }
+        };
+
+        let wei_wsl_server_version = format!("{}/version", wsl_url);
+
+        let data = ureq::get(&wei_wsl_server_version)
+            .set("Content-Type", "application/json")
+            .call()?.into_string()?;
+
+        if data != "wei-wsl-server" {
+            run_server()?;
+            continue;
+        }
+
+        let url = format!("{}/run", wsl_url);
+
+        let client = reqwest::blocking::Client::new();
+        let args: serde_json::Value = serde_json::from_str(args)?;
+
+        let response = client.post(url)
+            .json(&args)
+            .send()?;
+        
+        println!("{}", response.text()?);
+        return Ok(());
+    }
+    
+    Ok(())
+}
+
+fn run_server() -> Result<(), Box<dyn std::error::Error>> {
+    wei_run::command_async("wsl", vec!["wei-wsl-server"])?;
+    std::thread::sleep(std::time::Duration::from_secs(1));
     Ok(())
 }
